a1cf30c4fd784239c2cab6e92946bb3fe4f72ef8
bmarotta <brunom@kopf.com.br>
2022-11-04 19:09:33 +0100

    Add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export { API } from "./api";
-export { fetchJson } from "./fetch-toolkit";
+export { fetchJson, fetchSetHeader } from "./fetch-toolkit";
 export { AuthenticationProvider, BasicAuthenticationProvider, BearerAuthenticationProvider } from "./authentication";
 export { FetchLogger, FetchConsoleLogger } from "./logging";
 export { FetchDecorator, FetchError, FetchHandler, HttpMethod, RequestInitToolkit } from "./types";
 export { ParallelPromiseHandler } from "./parallel-promise-handler";
+export { HTTP_HEADER_CONTENT_TYPE, HTTP_HEADER_ACCEPT, HTTP_HEADER_ACCEPT_JSON, HTTP_HEADER_AUTHORIZATION } from "./constants";
 
