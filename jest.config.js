adf59fbde6ce689acbcf218a52e562cc1df39a07
Bruno Marotta <brunom@kopf.com.br>
2024-06-17 12:04:30 +0200

    Fix coverage badge generation

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
     // Replace `ts-jest` with the preset you want to use
     // from the above list
     preset: "ts-jest",
-    collectCoverageFrom: ["<rootDir>/packages/**/*.{tsx,ts}", "!**/*.{js,d.ts}"],
+    collectCoverageFrom: ["<rootDir>/src/**/*.{tsx,ts}", "!**/*.{js,d.ts}"],
     coverageReporters: ["lcov", "json-summary"],
 };
